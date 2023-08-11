47e35cbc0cb47354e6d1cd31e78501acd0b9d518
alaskannate <64567768+alaskannate@users.noreply.github.com>
2023-08-10 17:41:57 -0700

    inital commit

---
+++ src/calculations.js
@@ -0,0 +1,33 @@
+
+
+// Takes the inputs of a and b (the two shortest sides of a triangle).
+// Outputs the Hypotenuse, or the longest side of the triangle. 
+const calculateHypotenuse = (a, b) => {
+    const cSquared = (a * a) + (b * b);
+    const c = Math.sqrt(cSquared);
+    return c
+};
+
+console.log(calculateHypotenuse(3, 5))
+
+
+// Temp and unit of mesurement must pair, for exsample...
+// convertTemperature(100, C) would convert to what 100C would equial in F, ie. 212F. 
+const convertTemperature = (temp, scale) => {
+        if (scale == 'c' || scale == 'C') {
+            return ((temp * 1.8) + 32)
+        }
+        else if (scale == 'f' || scale == 'F') {
+            return ((temp - 32) * .555555)
+        } 
+        else {
+            console.log("Unit of measurement, or 'scale' must be writen as either C or F.")
+        }};
+        
+console.log(convertTemperature(100, 'c'))
+
+
+
+export default calculateHypotenuse
+
+export {convertTemperature}
---
+++ src/components/List.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import calculateHypotenuse, {convertTemperature,} from "../calculations.js"
+
+function List() {
+  return (
+<div className="calc-box">
+<div className="calc">
+        <ul>
+      <li>{convertTemperature(100,"c")}</li>
+      <li>{calculateHypotenuse(2,4)}</li>
+        </ul>
+        <ul>
+      <li>{convertTemperature(100,"c")}</li>
+      <li>{calculateHypotenuse(2,4)}</li>
+        </ul>
+        <ul>
+      <li>{convertTemperature(100,"c")}</li>
+      <li>{calculateHypotenuse(2,4)}</li>
+        </ul>
+        <ul>
+      <li>{convertTemperature(100,"c")}</li>
+      <li>{calculateHypotenuse(2,4)}</li>
+        </ul>
+        <ul>
+      <li>{convertTemperature(100,"c")}</li>
+      <li>{calculateHypotenuse(2,4)}</li>
+        </ul>
+    </div>
+</div>
+
+  );
+}
+
+export default List;
